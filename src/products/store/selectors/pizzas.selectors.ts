0f3fcb2cb51462a89e97acde04b4f7fb5332929e
Jarno <127001@users.noreply.github.com>
2023-01-09 13:42:57 +0100

    Mapping IDs to Entities

--- src/products/store/selectors/pizzas.selectors.ts
+++ src/products/store/selectors/pizzas.selectors.ts
@@ -1,12 +1,11 @@
-import { createSelector } from '@ngrx/store';
+import { createSelector } from "@ngrx/store";
 
-import * as fromRoot from '../../../app/store';
-import * as fromFeature from '../reducers';
-import * as fromPizzas from '../reducers/pizzas.reducer';
-import * as fromToppings from '../reducers/toppings.reducer';
+import * as fromRoot from "../../../app/store";
+import * as fromFeature from "../reducers";
+import * as fromPizzas from "../reducers/pizzas.reducer";
+import * as fromToppings from "./toppings.selectors";
 
-import { Pizza } from '../../models/pizza.model';
-
+import { Pizza } from "../../models/pizza.model";
 
 // pizzas state
 export const getPizzaState = createSelector(
@@ -23,7 +22,7 @@ export const getSelectedPizza = createSelector(
   getPizzasEntities,
   fromRoot.getRouterState,
   (entities, router): Pizza => {
-    return router.state && entities[router.state.params.pizzaId]
+    return router.state && entities[router.state.params.pizzaId];
   }
 );
 
@@ -32,19 +31,14 @@ export const getPizzaVisualised = createSelector(
   fromToppings.getToppingEntities,
   fromToppings.getSelectedToppings,
   (pizza, toppingEntities, selectedToppings) => {
-    const toppings = selectedToppings.map(id => toppingEntities[id]);
-    return {
-      ...pizza, toppings
-    };
+    const toppings = selectedToppings.map((id) => toppingEntities[id]);
+    return { ...pizza, toppings };
   }
 );
 
-export const getAllPizzas = createSelector(
-  getPizzasEntities,
-  (entities) => {
-    return Object.keys(entities).map(id => entities[parseInt(id, 10)]);
-  }
-)
+export const getAllPizzas = createSelector(getPizzasEntities, (entities) => {
+  return Object.keys(entities).map((id) => entities[parseInt(id, 10)]);
+});
 
 export const getAllPizzasLoaded = createSelector(
   getPizzaState,
