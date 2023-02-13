fdd70732c0efbba9157d96c291b12ac621509ddf
Jarno <127001@users.noreply.github.com>
2023-02-13 12:14:30 +0100

    Preloading across multiple Routes

--- src/products/store/selectors/pizzas.selectors.ts
+++ src/products/store/selectors/pizzas.selectors.ts
@@ -1,13 +1,12 @@
-import { createSelector } from "@ngrx/store";
+import { createSelector } from '@ngrx/store';
 
-import * as fromRoot from "../../../app/store";
-import * as fromFeature from "../reducers";
-import * as fromPizzas from "../reducers/pizzas.reducer";
-import * as fromToppings from "./toppings.selectors";
+import * as fromRoot from '../../../app/store';
+import * as fromFeature from '../reducers';
+import * as fromPizzas from '../reducers/pizzas.reducer';
+import * as fromToppings from './toppings.selectors';
 
-import { Pizza } from "../../models/pizza.model";
+import { Pizza } from '../../models/pizza.model';
 
-// pizzas state
 export const getPizzaState = createSelector(
   fromFeature.getProductsState,
   (state: fromFeature.ProductsState) => state.pizzas
@@ -31,20 +30,20 @@ export const getPizzaVisualised = createSelector(
   fromToppings.getToppingEntities,
   fromToppings.getSelectedToppings,
   (pizza, toppingEntities, selectedToppings) => {
-    const toppings = selectedToppings.map((id) => toppingEntities[id]);
+    const toppings = selectedToppings.map(id => toppingEntities[id]);
     return { ...pizza, toppings };
   }
 );
 
-export const getAllPizzas = createSelector(getPizzasEntities, (entities) => {
-  return Object.keys(entities).map((id) => entities[parseInt(id, 10)]);
+export const getAllPizzas = createSelector(getPizzasEntities, entities => {
+  return Object.keys(entities).map(id => entities[parseInt(id, 10)]);
 });
 
-export const getAllPizzasLoaded = createSelector(
+export const getPizzasLoaded = createSelector(
   getPizzaState,
   fromPizzas.getPizzasLoaded
 );
-export const getAllPizzasLoading = createSelector(
+export const getPizzasLoading = createSelector(
   getPizzaState,
   fromPizzas.getPizzasLoading
 );
